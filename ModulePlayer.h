9f5c7cd8ab9ef4a36e4afb150aeabe64b1447622
christianpi4 <chricraft4100@gmail.com>
2019-04-28 13:39:06 +0200

    Añadido Ltimer y Htimer

--- ModulePlayer.h
+++ ModulePlayer.h
@@ -13,14 +13,17 @@
 
 #define MAX_KEYS 300
 
-#define JUMP_TIME 1000
-#define PUNCH_TIME 400
+//#define JUMP_TIME 1000
+//#define PUNCH_TIME 400
 
 #define JUMP_TIME 1020
 #define PUNCH_TIME 250
 
 #define SP1_TIME 500
 #define KICK_TIME 550
+
+#define LDAMAGE_TIME 100
+#define HDAMAGE_TIME 200
 
 struct SDL_Texture;
 
@@ -67,6 +70,8 @@ public:
 	Uint32 punch_timer = 0;
 	Uint32 sp1_timer = 0;
 	Uint32 kick_timer = 0;
+	Uint32 ldamage_timer = 0;
+	Uint32 hdamage_timer = 0;
 
 	
 	bool animdone = true;
