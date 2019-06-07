57ce19f089e1016b3675fbdcfbef2d5cc203a796
Javier Belda González <33731319+JBGelsenorJ@users.noreply.github.com>
2019-06-07 13:22:09 +0200

    warnings solved

--- ModuleParticles.h
+++ ModuleParticles.h
@@ -44,7 +44,8 @@ public:
 
 private:
 
-	SDL_Texture * graphics = nullptr;
+	SDL_Texture * graphics = nullptr; 
+	SDL_Texture * graphics2 = nullptr;
 	Particle* active[MAX_ACTIVE_PARTICLES];
 	uint last_particle = 0;
 
