4dfce643be94f4eb5759aec12f923b79ece99e5d
christianpi4 <chricraft4100@gmail.com>
2019-06-09 11:37:36 +0200

    Added Crouch collider

--- ModulePlayer2.cpp
+++ ModulePlayer2.cpp
@@ -247,6 +247,7 @@ bool ModulePlayer2::Start()
 	initialPos = position.y;
 
 	playercol = App->collision->AddCollider({ 50, -250, 45, -103 }, COLLIDER_PLAYER, this);
+	playercrouch = App->collision->AddCollider({ 50, -250, 45, -60 }, COLLIDER_PLAYER, this);
 	playerpunch = App->collision->AddCollider({ 0, 0, 0, 0 }, COLLIDER_PLAYER_SHOT, 0);
 	playerkick = App->collision->AddCollider({ 0, 0, 0, 0 }, COLLIDER_PLAYER_SHOT, 0);
 	playercrouchkick = App->collision->AddCollider({ 0, 0, 0, 0 }, COLLIDER_PLAYER_SHOT, 0);
@@ -832,6 +833,7 @@ update_status ModulePlayer2::Update()
 	SDL_Rect* r = &current_animation->GetCurrentFrame();
 
 	playercol->SetPos(position.x, position.y);
+	playercrouch->SetPos(position.x, position.y);
 	if (App->enemy2->position.x > position.x)
 	{
 		App->render->Blit(graphics, position.x + (current_animation->pivotx2[current_animation->returnCurrentFrame()]), position.y - r->h + current_animation->pivoty2[current_animation->returnCurrentFrame()], r);
@@ -865,6 +867,7 @@ update_status ModulePlayer2::Update()
 	}
 
 	playercol->SetPos(position.x, position.y);
+	playercrouch->SetPos(position.x, position.y);
 
 	return UPDATE_CONTINUE;
 
@@ -1042,7 +1045,7 @@ player_states ModulePlayer2::process_fsm(p2Qeue<player_inputs>& inputs)
 		case ST_CROUCH:
 		{
 			playercol->to_delete = true;
-			playercol = App->collision->AddCollider({ 50, -250, 45, -65 }, COLLIDER_PLAYER, this);
+			
 			switch (last_input)
 			{
 			
